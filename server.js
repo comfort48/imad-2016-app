0b42c8a1e5fa91260dd60aa35653cc6d4156d726
comfort48 <aseesh.mullapudi@gmail.com>
2016-09-22 20:50:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content={
+    'BREAKING-BAD'={
+        title
+    }
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
