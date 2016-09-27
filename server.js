c6f2fe7bc425af0d073897e8943a3d9d3476e6c0
Aseesh <aseesh@aseesh.mullapudi>
2016-09-27 10:13:14 +0530

    added likes counter to profile

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
-
-
+var counter=0;
+app.get('/ui/counter',function(req,res)
+{
+  counter=counter+1;
+  res.send(counter.toString());
+});
+app.get('/ui/main.js',function(req,res)
+{
+res.sendFile(path.join(__dirname,'ui','main.js'));
+});
 var series={ 
 'BREAKING-BAD':{ 
   title:'BREAKING-BAD|SERIES',
@@ -49,12 +56,10 @@ app.get('/:seriesName', function (req, res) {
    var seriesName=req.params.seriesName;
   res.send(createTemplate(series[seriesName]));
 });
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -63,8 +68,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
