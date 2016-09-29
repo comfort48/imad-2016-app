e9bd082f593810b21b85948de4ffa887d52e02ce
comfort48 <aseesh.mullapudi@gmail.com>
2016-09-29 15:16:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,4 +24,28 @@ request.send(null);
 //FOR COMMENTS.
 
 
+var submit=document.getElementById('sub_btn');
+submit.onclick=function(){
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'<li>';
+                
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://comfort48.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+};
 
